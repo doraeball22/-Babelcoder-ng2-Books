9a80edbf879281302387ccf4c0d660e1ab30eede
doraeball22 <anurak_b@cmu.ac.th>
2017-08-16 06:30:25 +0700

    children routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { BookService } from './books/shared/book.service';
+import { BookListComponent } from './books/book-list/book-list.component'
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/book',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    BookListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    BookService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/books/books.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from './shared/book.service';
+import { Book } from './shared/book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
