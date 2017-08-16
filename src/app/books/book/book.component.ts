67311c82cb79e0fadf7728bdf272c4758a66629c
doraeball22 <anurak_b@cmu.ac.th>
2017-08-16 21:17:20 +0700

    Book Component

--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { Book } from '../shared/book';
+import { BookService } from '../shared/book.service';
 
 @Component({
   selector: 'app-book',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookComponent implements OnInit {
 
-  constructor() { }
+  book: Book;
+
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.setBook();
+  }
+
+  private setBook() {
+    const { id } = this.route.snapshot.params;
+    // +id is convert string to integer
+    this.book = this.bookService.getBook(+id);
   }
 
 }
